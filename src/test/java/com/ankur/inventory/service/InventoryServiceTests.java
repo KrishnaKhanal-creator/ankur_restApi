cddb84974482f8e87101912046ab37e97840a212
Ankur Israni <ankur.israni@aa.com>
2019-12-08 18:18:22 -0600

    first commit

--- src/test/java/com/ankur/inventory/service/InventoryServiceTests.java
+++ src/test/java/com/ankur/inventory/service/InventoryServiceTests.java
@@ -1,12 +1,26 @@
 package com.ankur.inventory.service;
 
 
+import com.ankur.inventory.domain.InventoryListAllResponse;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import org.springframework.http.ResponseEntity;
 
 public class InventoryServiceTests {
 
+    private InventoryService inventoryService;
+
     @Test
     public void test(){
+        inventoryService = mock(InventoryService.class);
+//        inventoryService = new InventoryService();
+
+//
+//        when(db.getPrice(googleStock)).thenReturn(50.00);
+//
+//        ResponseEntity<InventoryListAllResponse> result = (ResponseEntity<InventoryListAllResponse>)inventoryService.listAll();
+//        InventoryListAllResponse response = result.getBody();
+//        System.out.println("response = "+response);
 
     }
 }
