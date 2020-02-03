e21564289d9eaf8c69541d842500332cc81f4737
Ankur Israni <ankur.israni@aa.com>
2020-02-03 01:47:34 -0600

    First commit

--- src/test/java/com/ankur/inventory/service/InventoryServiceTests.java
+++ src/test/java/com/ankur/inventory/service/InventoryServiceTests.java
@@ -1,26 +1,35 @@
 package com.ankur.inventory.service;
 
 
-import com.ankur.inventory.domain.InventoryListAllResponse;
+import com.ankur.inventory.domain.*;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InventoryServiceTests {
 
     private InventoryService inventoryService;
 
     @Test
-    public void test(){
-        inventoryService = mock(InventoryService.class);
-//        inventoryService = new InventoryService();
-
-//
-//        when(db.getPrice(googleStock)).thenReturn(50.00);
-//
-//        ResponseEntity<InventoryListAllResponse> result = (ResponseEntity<InventoryListAllResponse>)inventoryService.listAll();
-//        InventoryListAllResponse response = result.getBody();
-//        System.out.println("response = "+response);
-
+    public void findById() {
+        InventoryService service = new InventoryService();
+        ResponseEntity<InventoryFindByIdResponse> response = (ResponseEntity<InventoryFindByIdResponse>) service.findById(new InventoryFindByIdRequest(1));
+        assertEquals("Lamp",response.getBody().getItem().getName());
     }
+
+    @Test
+    public void findByName() {
+        InventoryService service = new InventoryService();
+        ResponseEntity<InventoryFindByNameResponse> response = (ResponseEntity<InventoryFindByNameResponse>) service.findByName(new InventoryFindByNameRequest(("Lamp")));
+        Collection<Item> items =  response.getBody().getItems();
+        items.forEach(item->{
+            assertTrue(item.getName().contains("Lamp"));
+        });
+    }
+
+
 }
