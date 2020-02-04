ea3ee188ad1f95823eb936d79c4df539f39e30cb
Ankur Israni <ankur.israni@aa.com>
2020-02-04 08:21:14 -0600

    debugging

--- devops/basic_pipeline.groovy
+++ devops/basic_pipeline.groovy
@@ -1,32 +1,55 @@
 pipeline{
-    agent {any
+    agent any
+    tools {
+        /**This is configured with this exact name in 'Manage Jenkins > Global Tool Configuration > Maven'*/
+        maven 'maven_3_6_3'
+    }
 
-    stages{
-        stage("compile"){
+    stages {
+
+        stage('build'){
             steps{
-                echo 'Compiling............'
-                withMaven(maven: 'maven_3_5_0'){
+                echo 'Building............'
+                withMaven(maven: 'maven_3_6_3'){
                     sh 'mvn clean compile'
                 }
             }
-
-            stage("test"){
-                steps{
-                    echo 'Running tests............'
-                    withMaven(maven: 'maven_3_5_0'){
-                        sh 'mvn test'
-                    }
+        }
+        stage('test'){
+            steps{
+                echo 'Running tests............'
+                withMaven(maven: 'maven_3_6_3'){
+                    sh 'mvn test'
                 }
             }
+        }
 
+        stage('run'){
+            steps{
+                echo 'starting service..........'
+                withMaven(maven: 'maven_3_6_3'){
+                    //sh 'mvn spring-boot:stop'
+                    // sh 'mvn spring-boot:run'
+                    // echo "mvn spring-boot:run" | at now + 1 minutes
+                    echo "launching mvn spring-boot:run"
+                    //sh "timeout -s KILL 4m mvn spring-boot:run -Dpmd.skip=true -Dcpd.skip=true -Dfindbugs.skip=true || true"
+                    // sh "nohup mvn spring-boot:run || true &"
+                    sh "nohup mvn spring-boot:run & || true"
+                }
+            }
+        }
+
+
+/*
             stage('deploy'){
                 steps{
                     echo 'Deployment in progress............'
-                    withMaven(maven: 'maven_3_5_0'){
+                    withMaven(maven: 'maven_3_6_3'){
                         sh 'mvn deploy'
                     }
                 }
             }
-        }
+*/
     }
 }
+
